4ef0200e69fd877e756b925c8054d940cde427d6
John Downs <john.downs@microsoft.com>
2019-12-13 15:26:51 +1300

    Add scenario for simultaneous attempts at adding trackers to an order

--- DurableEntitiesItemTracker/TestOrchestrator.cs
+++ DurableEntitiesItemTracker/TestOrchestrator.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DurableEntitiesItemTracker.Entities;
+using Dynamitey.DynamicObjects;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DurableEntitiesItemTracker
@@ -31,7 +35,7 @@ namespace DurableEntitiesItemTracker
         {
             var orderItemId = $"Scenario1TestOrder{context.NewGuid().ToString()}";
             var quantity = 2;
-            var trackerIds = new[] { "Scenario1Tracker1", "Scenario1Tracker2" };
+            var trackerIds = new[] { $"Scenario1Tracker-{context.NewGuid().ToString()}-1", $"Scenario1Tracker-{context.NewGuid().ToString()}-2" };
 
             await TrackingOrchestrationFunctions.CreateOrderItem(orderItemId, quantity, context);
             if (!context.IsReplaying) log.LogInformation($"Order item entity {orderItemId} now exists, and has a quantity of {quantity}.");
@@ -62,10 +66,10 @@ namespace DurableEntitiesItemTracker
             [OrchestrationTrigger] IDurableOrchestrationContext context,
             ILogger log)
         {
-            var orderItem1Id = $"Scenario2TestOrder{context.NewGuid().ToString()}";
-            var orderItem2Id = $"Scenario2TestOrder{context.NewGuid().ToString()}";
+            var orderItem1Id = $"Scenario2TestOrder-{context.NewGuid().ToString()}";
+            var orderItem2Id = $"Scenario2TestOrder-{context.NewGuid().ToString()}";
             var quantity = 2;
-            var trackerId = "Scenario2Tracker";
+            var trackerId = $"Scenario2Tracker-{context.NewGuid().ToString()}";
 
             await TrackingOrchestrationFunctions.CreateOrderItem(orderItem1Id, quantity, context);
             if (!context.IsReplaying) log.LogInformation($"Order item entity {orderItem1Id} now exists, and has a quantity of {quantity}.");
@@ -90,9 +94,6 @@ namespace DurableEntitiesItemTracker
         }
         #endregion
 
-
-
-
         #region Scenario 3: Order already has enough trackers
         // This scenario creates an order with a quantity of 2, assigns two trackers to item within it, and then tries to assign another tracker to the order.
         // This scenario results in an exception.
@@ -113,7 +114,7 @@ namespace DurableEntitiesItemTracker
         {
             var orderItemId = $"Scenario3TestOrder{context.NewGuid().ToString()}";
             var quantity = 2;
-            var trackerIds = new[] { "Scenario3Tracker1", "Scenario3Tracker2" };
+            var trackerIds = new[] { $"Scenario3Tracker-{context.NewGuid().ToString()}-1", $"Scenario3Tracker-{context.NewGuid().ToString()}-2" };
 
             await TrackingOrchestrationFunctions.CreateOrderItem(orderItemId, quantity, context);
             if (!context.IsReplaying) log.LogInformation($"Order item entity {orderItemId} now exists, and has a quantity of {quantity}.");
@@ -126,7 +127,7 @@ namespace DurableEntitiesItemTracker
 
             try
             {
-                var anotherTrackerId = "Scenario3Tracker3";
+                var anotherTrackerId = $"Scenario3Tracker-{context.NewGuid().ToString()}-3";
                 await TrackingOrchestrationFunctions.ApplyTrackingConfiguration(orderItemId, anotherTrackerId, context);
 
                 // The above call should fail, since the order already has the maximum number of trackers.
@@ -138,5 +139,89 @@ namespace DurableEntitiesItemTracker
             }
         }
         #endregion
+
+        #region Scenario 4: Add multiple trackers simultaneously
+        // TODO
+        // This scenario creates an order with a quantity of 1, and then attempts to assign multiple trackers.
+        // Only one of these attempts succeeds due to the locking behaviour of the durable entities.
+        // To invoke this, issue a GET to http://localhost:7071/api/Scenario4
+
+        [FunctionName("Scenario4")]
+        public static async Task Scenario4EntryPoint(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]HttpRequest req,
+            [DurableClient] IDurableOrchestrationClient orchestrationClient)
+        {
+            await orchestrationClient.StartNewAsync(nameof(Scenario4Orchestrator));
+        }
+
+        [FunctionName(nameof(Scenario4Orchestrator))]
+        public static async Task Scenario4Orchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context,
+            ILogger log)
+        {
+            var orderItemId = $"Scenario4TestOrder-{context.NewGuid().ToString()}";
+            var quantity = 1;
+
+            await TrackingOrchestrationFunctions.CreateOrderItem(orderItemId, quantity, context);
+            if (!context.IsReplaying) log.LogInformation($"Order item entity {orderItemId} now exists, and has a quantity of {quantity}.");
+
+            // Send multiple simultaneous attempts to add a tracker to the order item.
+            var assignmentAttempts = 3;
+            var trackerAssignmentTasks = new List<Task>();
+            for (int i = 0; i < assignmentAttempts; i++)
+            {
+                trackerAssignmentTasks.Add(TrackingOrchestrationFunctions.ApplyTrackingConfiguration(orderItemId, $"Scenario4Tracker-{context.NewGuid().ToString()}-{i}", context));
+            }
+            var x = await Task.WhenAll(trackerAssignmentTasks.Select(task => WrapResultOrException(task)));
+
+            // Check how many succeeded and failed.
+            var successfulAttempts = trackerAssignmentTasks.Count(t => ! t.IsFaulted);
+            var failedAttempts = trackerAssignmentTasks.Count(t => t.IsFaulted);
+
+            if (!context.IsReplaying) log.LogInformation($"Out of {assignmentAttempts} attempts to place a tracker on the order item, {successfulAttempts} succeeded and {failedAttempts} failed.");
+        }
+        #endregion
+
+        private static async Task<TaskSucceededOrFailed> WrapResultOrException(Task task)
+        {
+            try
+            {
+                await task;
+                return new TaskSucceededOrFailed(true);
+            }
+            catch (Exception ex)
+            {
+                return new TaskSucceededOrFailed(false);
+            }
+        }
+
+        public class TaskSucceededOrFailed
+        {
+            public TaskSucceededOrFailed(bool isSuccess)
+            {
+                IsSuccess = isSuccess;
+            }
+
+            public bool IsSuccess { get; }
+        }
+
+        public class ResultOrException<T>
+        {
+            public ResultOrException(T result)
+            {
+                IsSuccess = true;
+                Result = result;
+            }
+
+            public ResultOrException(Exception ex)
+            {
+                IsSuccess = false;
+                Exception = ex;
+            }
+
+            public bool IsSuccess { get; }
+            public T Result { get; }
+            public Exception Exception { get; }
+        }
     }
 }
